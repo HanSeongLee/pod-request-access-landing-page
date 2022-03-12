7e63b315b81cb980fc64cd39bd674cfb2bb15ee1
HanSeong Lee <jkhanseong@gmail.com>
2022-03-12 18:34:58 +0900

    ADD | Hero section on tablet

--- components/HeroSection/index.tsx
+++ components/HeroSection/index.tsx
@@ -8,19 +8,23 @@ const HeroSection: React.FC = () => {
     return (
         <section className={styles.heroSection}>
             <Container className={styles.container}>
-                <h2 className={styles.title}>
+                <div className={styles.content}>
+                    <h2 className={styles.title}>
                     <span className={styles.highlight}>
                         Publish your podcasts
                     </span>
-                    everywhere.
-                </h2>
-                <p className={styles.description}>
-                    Upload your audio to Pod with a single click. We’ll then distribute your podcast to Spotify,
-                    Apple Podcasts, Google Podcasts, Pocket Casts and more!
-                </p>
+                        everywhere.
+                    </h2>
+                    <p className={styles.description}>
+                        Upload your audio to Pod with a single click. We’ll then distribute your podcast to Spotify,
+                        Apple Podcasts, Google Podcasts, Pocket Casts and more!
+                    </p>
 
-                <PartnerContainer className={styles.partnerContainer} />
-                <RequestAccessContainer />
+                    <div className={styles.bottomContainer}>
+                        <PartnerContainer className={styles.partnerContainer}/>
+                        <RequestAccessContainer className={styles.requestAccessContainer} />
+                    </div>
+                </div>
             </Container>
         </section>
     );
